bef36ecaf6c164b755182a2ce195875197ed33dc
omarevolving@gmail.com <Kingoz08x!>
2021-10-02 22:55:14 -0400

    fixed

--- thrones_be/api/urls.py
+++ thrones_be/api/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.getRoutes),
     path('thrones/', views.getBathrooms),
     path('thrones/<str:pk>/', views.getBathroom),
-
+    path('profiles/', views.getUsers),
+    path('profile/', views.getUser),
+    path('reviews/', views.getReviews),
 ]
--- thrones_be/api/views.py
+++ thrones_be/api/views.py
@@ -3,31 +3,51 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
 from .serializers import BathroomSerializer
-from thrones_be.models import Bathroom
-
+from .serializers import ProfileSerializer
+from .serializers import ReviewSerializer
+from thrones_be.models import Bathroom, Review
+from users.models import Profile
 @api_view(['GET'])
 def getRoutes(request):
 
     routes = [
 
-        {'GET':'/api/throne'},
-        {'GET':'/api/throne/id'},
-        {'GET':'/api/throne/id/vote'},
+        {'GET':'/api/thrones'},
+        {'GET':'/api/thrones/id'},
+        {'GET':'/api/thrones/id/vote'},
 
-        {'POST': '/api/users/token'},
-        {'POST': '/api/users/token/refresh'},
+        {'POST': '/api/profiles/'},
+        {'POST': '/api/profiles/id'},
 
     ]
     return Response(routes)
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def getBathrooms(request):
     bathrooms = Bathroom.objects.all()
     serializer = BathroomSerializer(bathrooms, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def getUsers(request):
+    profile = Profile.objects.all()
+    serializer = ProfileSerializer(profile, many = True)
+    return Response(serializer.data)
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def getUser(request,pk):
+    profile = Profile.objects.get(id=pk)
+    serializer = ProfileSerializer(profile, many = False)
     return Response(serializer.data)
 
 @api_view(['GET'])
 def getBathroom(request, pk):
     bathroom = Bathroom.objects.get(id=pk)
     serializer = BathroomSerializer(bathroom, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def getReviews(request):
+    reviews = Review.objects.all()
+    serializer = ReviewSerializer(reviews, many = True)
     return Response(serializer.data)
